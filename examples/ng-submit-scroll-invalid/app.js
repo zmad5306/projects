e771a6e8d3c63bcb817767e89ca4be8c69f4a1bb
zmad5306 <zachary.maddox@gmail.com>
2016-05-06 07:13:16 -0400

    cleanup

--- examples/ng-submit-scroll-invalid/app.js
+++ examples/ng-submit-scroll-invalid/app.js
@@ -5,8 +5,6 @@
     var self = this;
     self.test = 'some data';
     self.submit = function() {
-      console.log('Submitted!!!!');
-      console.log(self.element0);
     };
   }]);
 
@@ -15,17 +13,12 @@
         require: "?form",
         link: function(scope, element, attrs, form) {
           element.on("submit", function(event) {
-            console.log('Form submitted!!!!');
-
             if (form && !form.$valid) {
               var firstInvalid = element[0].querySelector('.ng-invalid');
-              console.log(firstInvalid);
 
               if (firstInvalid) {
                 firstInvalid.focus();
               }
-
-              console.log('Invalid form Submitted!!!!');
             }
           })
         }
