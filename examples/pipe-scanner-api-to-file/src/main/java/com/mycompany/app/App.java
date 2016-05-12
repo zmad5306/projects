2c81a2cf5128a871cb0b3d4a7db378f6283bc06c
zmad5306 <zachary.maddox@gmail.com>
2016-05-11 20:51:45 -0400

    added file splitting

--- examples/pipe-scanner-api-to-file/src/main/java/com/mycompany/app/App.java
+++ examples/pipe-scanner-api-to-file/src/main/java/com/mycompany/app/App.java
@@ -7,10 +7,14 @@ import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.URL;
 import java.net.URLConnection;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class App {
 
 	private static final String FEED_URL = "http://relay.broadcastify.com:80/768030603.mp3";
+	private static final SimpleDateFormat DATE_FORMAT = new SimpleDateFormat("yyyyMMddhhmmssSSS");
+	private static final int FILE_SPLIT_SIZE = 10485760;
 
 	public static void main(String[] args) throws IOException {
 		URL feedUrl = new URL(FEED_URL);
@@ -20,12 +24,19 @@ public class App {
 		OutputStream outstream = null;
 		try {
 			stream = connection.getInputStream();
-			outstream = new FileOutputStream(new File("test.mp3"));
+			outstream = new FileOutputStream(new File(generateOutFileName()));
 
 			byte[] buffer = new byte[4096];
 			int len;
+			int bytesRead = 0;
 			while ((len = stream.read(buffer)) != -1) {
+				bytesRead += len;
 				outstream.write(buffer, 0, len);
+				outstream.flush();
+				if (bytesRead > FILE_SPLIT_SIZE) {
+					outstream.close();
+					outstream = new FileOutputStream(new File(generateOutFileName()));
+				}
 			}
 		} finally {
 			if (null != stream ) { 
@@ -36,8 +47,9 @@ public class App {
 				outstream.close();
 			}
 		}
+	}
 
-		
-
+	private static String generateOutFileName() {
+		return DATE_FORMAT.format(new Date(System.currentTimeMillis())) + ".mp3";
 	}
 }
