c08fbf1839c75296f660ca1091502acd6c6ec94f
zmad5306 <zachary.maddox@gmail.com>
2016-05-11 20:20:36 -0400

    cleanup

--- examples/pipe-scanner-api-to-file/src/main/java/com/mycompany/app/App.java
+++ examples/pipe-scanner-api-to-file/src/main/java/com/mycompany/app/App.java
@@ -9,32 +9,31 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 
 public class App {
-	
+
 	private static final String FEED_URL = "http://relay.broadcastify.com:80/768030603.mp3";
-	
-    public static void main( String[] args ) throws IOException {
-    	URL feedUrl = new URL(FEED_URL);
-    	final HttpURLConnection connection = (HttpURLConnection) feedUrl.openConnection();
-    	InputStream stream = connection.getInputStream();
-    	OutputStream outstream = new FileOutputStream(new File("test.mp3"));
-    	
-    	Runtime.getRuntime().addShutdownHook(new Thread() {
+
+	public static void main(String[] args) throws IOException {
+		URL feedUrl = new URL(FEED_URL);
+		final HttpURLConnection connection = (HttpURLConnection) feedUrl.openConnection();
+		InputStream stream = connection.getInputStream();
+		OutputStream outstream = new FileOutputStream(new File("test.mp3"));
+
+		Runtime.getRuntime().addShutdownHook(new Thread() {
 			@Override
 			public void run() {
-				System.out.println("in shutdown hook....");
 				connection.disconnect();
 			}
 		});
-    	
-    	byte[] buffer = new byte[4096];
-    	int len;
-    	while((len = stream.read(buffer)) != -1) {
-    		stream.read(buffer);
-    		outstream.write(buffer, 0, len);
-    	}
-    	
-    	stream.close();
-    	outstream.close();
-    	
-    }
+
+		byte[] buffer = new byte[4096];
+		int len;
+		while ((len = stream.read(buffer)) != -1) {
+			stream.read(buffer);
+			outstream.write(buffer, 0, len);
+		}
+
+		stream.close();
+		outstream.close();
+
+	}
 }
