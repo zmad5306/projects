fa637c13380ed47bd154001ddea390de02f0e548
zmad5306 <zachary.maddox@gmail.com>
2016-09-30 21:46:41 -0400

    Refactored for routing.

--- angular-quickstart/app/app.module.ts
+++ angular-quickstart/app/app.module.ts
@@ -3,10 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule }   from '@angular/forms';
 import { AppComponent }   from './app.component';
 import { HeroDetailComponent } from './hero-detail.component';
+import { HeroesComponent } from './heroes.component';
+import { HeroService } from './hero.service';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HeroDetailComponent ],
+  declarations: [ AppComponent, HeroDetailComponent, HeroesComponent ],
+  providers: [
+    HeroService
+  ],
   bootstrap: [ AppComponent ]
 })
 
